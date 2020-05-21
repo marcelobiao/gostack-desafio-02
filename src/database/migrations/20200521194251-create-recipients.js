a01413ede1d79d54d9d83c324e7509568b185044
Marcelo Biao <marcelobiao2@gmail.com>
2020-05-21 17:24:42 -0300

    Corrigindo bug na migration de recipients

--- src/database/migrations/20200521194251-create-recipients.js
+++ src/database/migrations/20200521194251-create-recipients.js
@@ -17,7 +17,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references:{
-          model: 'user',
+          model: 'users',
           key: 'id'
         },
         onUpdate: 'CASCADE',

a5dd6521852ae8e7194cd9febf59e4393d1aec84
Marcelo Biao <marcelobiao2@gmail.com>
2020-05-22 17:57:51 -0300

    Implementando model de users

--- src/app/models/Users.js
+++ src/app/models/Users.js
@@ -1,9 +1,11 @@
 import Sequelize, {Model} from 'sequelize';
+import bycrypt from 'bcryptjs';
 
 class Users extends Model{
   static init(sequelize){
     super.init(
       {
+        name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
       },
@@ -17,6 +19,10 @@ class Users extends Model{
 
   static associate(models){
     this.hasMany(models.Recipients, {foreignKey: 'user_id', as: 'recipients'});
+  }
+
+  checkPassword(password){
+    return bycrypt.compare(password, this.password_hash);
   }
 }
 

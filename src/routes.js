c5028f08c20a2fd3422747dac3b9ae1ae5b5ab14
Marcelo Biao <marcelobiao2@gmail.com>
2020-05-30 20:09:57 -0300

    Refatorando rotas da api

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,8 @@
 import { Router } from 'express';
 
-import Users from './app/models/Users';
-import Recipients from './app/models/Recipients';
-
 import SessionController from './app/controllers/SessionController';
 import UsersController from './app/controllers/UserController';
 import AuthMiddleware from './app/middlewares/AuthMiddleware';
-
 
 const routes = new Router();
 
